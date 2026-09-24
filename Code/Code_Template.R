42a960df34cbf52bbc331cda9adaafd333bee4f3
syeducation <moin@umn.edu>
2026-09-24 06:25:01 -0700

    created file

---
+++ Code/Code_Template.R
@@ -0,0 +1,31 @@
+#### Project Name ####
+# Script Purpose
+# Created on DATE, by NAME
+# Major updates on DATE, by NAME (if relevant)
+# Checked on DATE, by NAME
+
+#############################################
+
+#### Workspace setup ####
+
+# Package loading
+# Directory paths (if relevant)
+# Session info with version numbers for R, R Studio, and all packages
+# Settings (e.g., settings seeds, digits; if relevant)
+# Check/set working directory
+
+#############################################
+
+#### Data import ####
+
+#############################################
+
+#### Data preparation ####
+
+#############################################
+
+#### <Code Section 1> ####
+
+#############################################
+
+#### <Code Section 2> ####
